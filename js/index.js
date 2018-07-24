1d5c9b2e4b409b336e4acebf29ef885e8ca8eb7a
alyona706 <almokina@gmail.com>
2018-07-24 14:09:28 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 (function () {
-    'use strict'
+    'use strict';
 
     if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-        var msViewportStyle = document.createElement('style')
+        var msViewportStyle = document.createElement('style');
         msViewportStyle.appendChild(
             document.createTextNode(
                 '@-ms-viewport{width:auto!important}'
             )
-        )
+        );
         document.head.appendChild(msViewportStyle)
     }
-    $(document).ready(function() {
+    $(document).ready(function () {
         //Set the carousel options
         $('#quote-carousel').carousel({
             pause: true,
-            interval: 8000,
+            interval: 8000
         });
     });
+    $(document).ready(function () {
+        $('.icon').click(function () {
+            $('.icon').toggleClass('active');
+        })
+    });
+    $(document).ready(function () {
+        $('.icon').click(function () {
+            $('.menu ul').toggleClass('open');
+        })
+    })
 }());
 // When the DOM is ready, run this function
